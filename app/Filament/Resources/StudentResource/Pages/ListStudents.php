a7722fd70ecdf406be38fd1e5bb8885d8f33ca30
rizaljihadudin <mithnyong@gmail.com>
2025-02-06 23:55:19 +0700

    feat: import excel students

--- app/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -3,13 +3,16 @@
 namespace App\Filament\Resources\StudentResource\Pages;
 
 use App\Filament\Resources\StudentResource;
+use App\Models\Student;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListStudents extends ListRecords
 {
     protected static string $resource = StudentResource::class;
+    public $file;
 
     protected function getHeaderActions(): array
     {
@@ -20,6 +23,14 @@ class ListStudents extends ListRecords
 
     public function getHeader(): ?View
     {
-        return view('filament.custom.upload-file');
+        $data = Actions\CreateAction::make();
+        return view('filament.custom.upload-file', compact('data'));
+    }
+
+    public function save()
+    {
+        if($this->file != null){
+            Excel::import(new ImportStudents(), $this->file);
+        }
     }
 }
