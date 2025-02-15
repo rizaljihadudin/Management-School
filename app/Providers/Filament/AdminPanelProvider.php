df8b3d81352bb64612b1eec37a43d21779df8304
rizaljihadudin <mithnyong@gmail.com>
2025-02-15 16:46:23 +0700

    feat: notifications for admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -139,6 +139,7 @@ class AdminPanelProvider extends PanelProvider
                         ]),
                 ]);
             })
+            ->databaseNotifications()
             ->viteTheme('resources/css/filament/admin/theme.css');
     }
 }
